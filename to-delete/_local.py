34d1a1e9eb5f47f96a08e197e36202f1266b8c57
Dennis A. Mertens <da.mertens.velasquez@outlook.com>
2025-05-29 04:49:05 +0200

    Update.

--- to-delete/_local.py
+++
@@ -1,6 +0,0 @@
-import _global
-from _mod import modify
-
-print(_global.a_variable)
-modify()
-print(_global.a_variable)
--- to-delete/_mod.py
+++
@@ -1,5 +0,0 @@
-import _global
-
-
-def modify() -> None:
-    _global.a_variable = 1
