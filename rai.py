b9d565caa93f5f54cd55754ba840757487980356
Dennis A. Mertens <da.mertens.velasquez@outlook.com>
2025-06-08 03:36:58 +0200

    Update rai.py

--- rai.py
+++ rai.py
@@ -1,24 +1,30 @@
 from torch.nn import Module, Parameter, MSELoss, Linear, LazyLinear
 from torch import Tensor
-from typing import Any, cast, Self
+from typing import Any, cast, Self, Literal
 from collections.abc import Callable
+from math import inf
 
 
 class RAI(Module):
     _forward: Module
     _loss_fn: Module
+    _period: int | float
 
-    def __init__(self, func: Module, loss_fn: Module = MSELoss()) -> None:
+    def __init__(self, func: Module, loss_fn: Module = MSELoss(), period: int | float = inf) -> None:
         super().__init__() # type: ignore
         self._forward = func
         self._loss_fn = loss_fn
+        self._period = period
     
     def forward(self, *arg: Any, **kwg: Any) -> Tensor:
         forward = cast(Callable[..., Tensor], self._forward)
         if self.training:
-            if not hasattr(self, "target"):
+            if not hasattr(self, "count"):
+                self.count = 0
+            if not hasattr(self, "target") or self.count % self._period == 0:
                 self.target = Parameter(forward(*arg, **kwg).detach())
             self.loss = self._loss_fn(self.target, forward(*arg, **kwg))
+            self.count += 1
             return self.target
         return forward(*arg, **kwg)
     
@@ -29,14 +35,16 @@ class RAI(Module):
                 del self.target
             if hasattr(self, "loss"):
                 del self.loss
+            if hasattr(self, "count"):
+                del self.count
         return self
     
     def pause(self, mode: bool = True) -> Self:
-        self.train(not mode)
+        super().train(not mode)
         for func in self.modules():
             if isinstance(func, RAI):
                 func.pause(mode)
-        return self    
+        return self
 
 
 def loss(func: Module) -> float | Tensor:
